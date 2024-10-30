7b13cd5206e9a7b3e51436d0d7146c738a17e396
dongxu <i@huangdx.net>
2024-10-30 10:58:28 -0700

    add theme

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function toggleTheme(theme) {
     document.body.classList.remove('light-theme', 'dark-theme');
     document.body.classList.add(`${theme}-theme`);
+
+    localStorage.setItem('theme', theme);
 }
+
+window.addEventListener('load', () => {
+    const savedTheme = localStorage.getItem('theme') || 'dark';
+    document.body.classList.add(`${savedTheme}-theme`);
+});
 
